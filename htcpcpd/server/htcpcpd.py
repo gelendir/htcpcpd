791f69c849b192a8c25e68e66289421d8ccf3544
Frédérik Paradis <fredy_14@live.fr>
2012-01-29 17:35:27 -0500

    Beginning of BREW implementation

--- htcpcpd/server/htcpcpd.py
+++ htcpcpd/server/htcpcpd.py
@@ -7,7 +7,12 @@ PORT = 8000
 TEAPOT = False
 
 class HTCPCPDImpl(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	
+	def __init__(self, *args, **kwargs):
+		SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
+
+	def __del__(self):
+		pass
+
 	def do_GET(self):
 		if self.isBadHTCPCPRequest():
 			return
@@ -15,7 +20,7 @@ class HTCPCPDImpl(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_POST(self):
 		if self.isBadHTCPCPRequest():
 			return
-
+		self.do_BREW()
 
 	def do_PROPFIND(self):
 		if self.isBadHTCPCPRequest():
@@ -30,6 +35,27 @@ class HTCPCPDImpl(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_BREW(self):
 		if self.isBadHTCPCPRequest():
 			return
+		
+		#print self.headers.getheader('content-type')
+
+		length = int(self.headers.getheader('content-length'))        
+		data_string = self.rfile.read(length)
+		if data_string not in ["start", "stop"]:
+			self.send_error(400, 'The data must be "start" or "stop" for this method.')
+			self.end_headers()
+			return
+		elif self.headers.getheader('content-type').strip() != "message/coffeepot":
+			self.send_error(400, "The content-type header is not correctly setted.")
+			self.end_headers()
+			return
+			
+		self.send_response(200)
+		self.end_headers()
+
+		if data_string == "start":
+			pass
+		elif data_string == "stop":
+			pass
 
 	def isBadHTCPCPRequest(self):
 		return self.isTeapot() or self.isNotAcceptable()
@@ -41,7 +67,7 @@ class HTCPCPDImpl(SimpleHTTPServer.SimpleHTTPRequestHandler):
 		return TEAPOT
 
 	def isNotAcceptable(self):
-		accept_additions = self.headers.getrawheader("Accept-Additions")
+		accept_additions = self.headers.getheader("Accept-Additions")
 		if accept_additions is not None and accept_additions.strip():
 			self.send_error(406, "Not Acceptable")
 			self.end_headers()
@@ -54,8 +80,8 @@ try:
 	
 	print "serving at port", PORT
 	httpd.serve_forever()
-except Exception:
-	pass
+except KeyboardInterrupt:
+	httpd.socket.close() 
 finally:
 	httpd.server_close()
 
