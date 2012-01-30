5f5a4780c469ddb20f78ee87d2089bcac026aa51
Frédérik Paradis <fredy_14@live.fr>
2012-01-30 16:03:17 -0500

    Adding some command in the cli client

--- htcpcp-client/client.py
+++ htcpcp-client/client.py
@@ -10,26 +10,52 @@ class HTCPCPClient(Cmd):
 		self.url = 'http://' + host[0] + ':' + str(host[1]) + '/'
 
 	def do_brew(self, line):
+		""" 
+		Start the coffee brewing. There is an error
+		if the coffee brewing is already started.
+		"""
 		self.brew_command("start")
 
 	def do_stop(self, line):
+		""" 
+		Stop the coffee brewing. There is an error
+		if the coffee brewing is already stopped.
+		"""
 		self.brew_command("stop")
 
 	def do_status(self, line):
-		pass
+		""" 
+		Show the brewing status of the coffee pot.
+		"""
+		self.curl_query("status")
 	
 	def do_water(self, line):
-		pass
+		""" 
+		Show the quantity of water in litre in the
+		coffee pot.
+		"""
+		self.curl_query("water")
 	
 	def do_bucket(self, line):
+		""" 
+		Show if the bucket is in the coffee pot or not.
+		"""
+		self.curl_query("bucket")
+
+	def do_resume(self, line):
+		""" 
+		Show many information on the coffee pot.
+		"""
 		pass
-	
+
 	def do_quit(self, line):
+		"""
+		Exit the HTCPCP command line client.
+		"""
 		return True
 
 	do_exit = do_quit
 	do_EOF = do_quit
-	help_EOF = do_quit
 
 	def brew_command(self, command):
 		c = Curl()
@@ -38,6 +64,12 @@ class HTCPCPClient(Cmd):
 		c.setopt(POST, 1)
 		c.setopt(POSTFIELDS, command) 
 		c.setopt(HTTPHEADER, ["Content-Type: message/coffeepot"])
+		print self.get_line(c)
+		c.close()
+
+	def curl_query(self, page):
+		c = Curl()
+		c.setopt(URL, self.url + page)
 		print self.get_line(c)
 		c.close()
 
